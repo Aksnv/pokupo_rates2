8f2ceca1d82488048d76ffd8c1d674d38d9528df
Yuri Aksenov <aksenov.mailbox@gmail.com>
2017-10-25 14:19:03 +0300

    Индивидуальные тарифы

--- scripts/main.js
+++ scripts/main.js
@@ -1,85 +1,13 @@
-/* Слайдер для тарифов */
+/* Новые тарифы */
 
-(function () {
-  
-  var leftArrow = document.querySelector(".rates_slide__ctrl-left");
-  var rightArrow = document.querySelector(".rates_slide__ctrl-right");
+$(".accordeon__trigger").click(function() {
+  if ($(this).hasClass("accordeon__trigger--close")) {
+    $(this).removeClass("accordeon__trigger--close");
+    $(".rates__content--individual").slideDown();
+  } else {
+    $(this).addClass("accordeon__trigger--close");
+    $(".rates__content--individual").slideUp();
+  }
+});
 
-  var screenWidth = document.documentElement.clientWidth;
-
-  window.addEventListener("resize", function(event) {
-
-    if (screenWidth > 1023) {
-      document.querySelector(".startup .grade-1").style = "block";
-      document.querySelector(".startup .grade-2").style = "block";
-      document.querySelector(".startup .grade-3").style = "block";
-      document.querySelector(".business .grade-1").style = "block";
-      document.querySelector(".business .grade-2").style = "block";
-      document.querySelector(".business .grade-3").style = "block";
-      document.querySelector(".company .grade-1").style = "block";
-      document.querySelector(".company .grade-2").style = "block";
-      document.querySelector(".company .grade-3").style = "block";
-    } else {
-      document.querySelector(".startup .grade-1").style = "none";
-      document.querySelector(".startup .grade-2").style = "block";
-      document.querySelector(".startup .grade-3").style = "none";
-      document.querySelector(".business .grade-1").style = "none";
-      document.querySelector(".business .grade-2").style = "block";
-      document.querySelector(".business .grade-3").style = "none";
-      document.querySelector(".company .grade-1").style = "none";
-      document.querySelector(".company .grade-2").style = "block";
-      document.querySelector(".company .grade-3").style = "none";
-
-      rightArrow.removeAttribute("disabled");
-      leftArrow.removeAttribute("disabled");
-    }
-
-  });
-
-  rightArrow.addEventListener("click", function(event) {
-
-    if (document.querySelector(".startup .grade-2").style.display == "block") {
-      document.querySelector(".startup .grade-2").style.display = "none";
-      document.querySelector(".startup .grade-3").style.display = "block";
-      document.querySelector(".business .grade-2").style.display = "none";
-      document.querySelector(".business .grade-3").style.display = "block";
-      document.querySelector(".company .grade-2").style.display = "none";
-      document.querySelector(".company .grade-3").style.display = "block";
-      rightArrow.setAttribute("disabled", "disabled");
-    } else {
-      document.querySelector(".startup .grade-1").style.display = "none";
-      document.querySelector(".startup .grade-2").style.display = "block";
-      document.querySelector(".business .grade-1").style.display = "none";
-      document.querySelector(".business .grade-2").style.display = "block";
-      document.querySelector(".company .grade-1").style.display = "none";
-      document.querySelector(".company .grade-2").style.display = "block";
-      leftArrow.removeAttribute("disabled");
-    }
-
-  });
-
-  leftArrow.addEventListener("click", function(event) {
-
-    if (document.querySelector(".startup .grade-2").style.display == "block") {
-      document.querySelector(".startup .grade-2").style.display = "none";
-      document.querySelector(".startup .grade-1").style.display = "block";
-      document.querySelector(".business .grade-2").style.display = "none";
-      document.querySelector(".business .grade-1").style.display = "block";
-      document.querySelector(".company .grade-2").style.display = "none";
-      document.querySelector(".company .grade-1").style.display = "block";
-      leftArrow.setAttribute("disabled", "disabled");
-    } else {
-      document.querySelector(".startup .grade-3").style.display = "none";
-      document.querySelector(".startup .grade-2").style.display = "block";
-      document.querySelector(".business .grade-3").style.display = "none";
-      document.querySelector(".business .grade-2").style.display = "block";
-      document.querySelector(".company .grade-3").style.display = "none";
-      document.querySelector(".company .grade-2").style.display = "block";
-      rightArrow.removeAttribute("disabled");
-    }
-
-  });
-
-}());
-
-/* END - Слайдер для тарифов */
+/* END - Новые тарифы */
